a5c1063e8b2aff516f0681f83e9b57cdd8e492c9
baegjonghyeon <baek011226@naver.com>
2025-08-22 10:59:21 +0900

    feat: 유저 이름으로 사용자 조회

--- src/main/java/com/example/springsecurityjwt/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/springsecurityjwt/domain/user/repository/UserRepository.java
@@ -3,6 +3,10 @@ package com.example.springsecurityjwt.domain.user.repository;
 import com.example.springsecurityjwt.domain.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     boolean existsByUsername(String username);
+
+    Optional<UserEntity> findByUsername(String username);
 }
