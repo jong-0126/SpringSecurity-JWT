cb08d07b4a29ae0c4805f8f2fee799fdf5b61e8d
baegjonghyeon <baek011226@naver.com>
2025-08-22 10:59:01 +0900

    feat: 로그인, 회원 조회 기능 추가

--- src/main/java/com/example/springsecurityjwt/domain/user/contorller/UserController.java
+++ src/main/java/com/example/springsecurityjwt/domain/user/contorller/UserController.java
@@ -1,14 +1,15 @@
 package com.example.springsecurityjwt.domain.user.contorller;
 
+import com.example.springsecurityjwt.domain.user.dto.LoginRequestDto;
+import com.example.springsecurityjwt.domain.user.dto.Token;
 import com.example.springsecurityjwt.domain.user.dto.UserRequestDto;
 import com.example.springsecurityjwt.domain.user.dto.UserResponseDto;
+import com.example.springsecurityjwt.domain.user.entity.UserEntity;
 import com.example.springsecurityjwt.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -18,7 +19,18 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<UserResponseDto> register(@RequestBody UserRequestDto requestDto) throws IllegalAccessException {
+    public ResponseEntity<UserResponseDto> register(@RequestBody UserRequestDto requestDto) {
         return ResponseEntity.ok(userService.register(requestDto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Token> login(@RequestBody LoginRequestDto requestDto) {
+
+        return ResponseEntity.ok(userService.login(requestDto.getUsername(), requestDto.getPassword()));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserResponseDto> getMyInfo(@AuthenticationPrincipal UserEntity user){
+        return ResponseEntity.ok(new UserResponseDto(user.getId(), user.getUsername(), user.getUserRole()));
     }
 }
