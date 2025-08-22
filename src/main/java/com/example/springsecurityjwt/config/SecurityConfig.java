76777a1b49c263db607dda19bbd1cd9c42879bf2
baegjonghyeon <baek011226@naver.com>
2025-08-22 10:58:24 +0900

    feat: 필터 등록

--- src/main/java/com/example/springsecurityjwt/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurityjwt/config/SecurityConfig.java
@@ -1,16 +1,22 @@
 package com.example.springsecurityjwt.config;
 
+import com.example.springsecurityjwt.domain.jwt.JwtFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtFilter jwtFilter;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
@@ -22,7 +28,9 @@ public class SecurityConfig {
                 .requestMatchers("/users/register", "/users/login").permitAll()
                 .anyRequest().authenticated()
             .and()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
