5302864067d027a2a7fc3b306cd07f2b532f251c
baegjonghyeon <baek011226@naver.com>
2025-08-27 16:23:03 +0900

    feat: admin limit 테스트 api 추가

--- src/main/java/com/example/springsecurityjwt/domain/test/controller/TestController.java
+++ src/main/java/com/example/springsecurityjwt/domain/test/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.example.springsecurityjwt.domain.test.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,5 +14,11 @@ public class TestController {
     @PostMapping("/hello")
     public ResponseEntity<String> rateLimitTest() {
         return ResponseEntity.ok("Hello, world!!");
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String admin() {
+        return "Admin zone";
     }
 }
